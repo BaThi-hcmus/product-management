3659885c0ed969982667aca7b3657e2f7cae4048
Huỳnh Bá Thi <huynhbathi2006@gmail.com>
2025-09-07 21:19:50 +0700

    update code

--- routes/admin/product-route.js
+++ routes/admin/product-route.js
@@ -2,7 +2,17 @@ const product_controller = require("../../controllers/admin/product-controller.j
 const multer  = require('multer');
 const express = require("express")
 const storage = require("../../helper/multer.js")
-const upload = multer({ storage: storage() })
+//const upload = multer({ storage: storage() })
+const upload = multer()
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+
+cloudinary.config({ 
+  cloud_name: 'dpll1onh7', 
+  api_key: '447859212893887', 
+  api_secret: '447859212893887'
+});
+
 const routes = express.Router()
 const product_validate = require("../../validate/admin/product.js")
 
@@ -21,6 +31,30 @@ routes.get("/create", product_controller.create)
 routes.post(
     "/create",
     upload.single('thumbnail'),
+    function (req, res, next) {
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+              (error, result) => {
+                if (result) {
+                  resolve(result);
+                } else {
+                  reject(error);
+                }
+              }
+            );
+
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+    }
+
+    upload(req);
+},
     product_validate.createPost,
     product_controller.createPost)
 
