30b5a23269f7217a0d97e6ab81af38f4db46d7c3
Huỳnh Bá Thi <huynhbathi2006@gmail.com>
2025-09-07 20:47:33 +0700

    update code

--- helper/multer.js
+++ helper/multer.js
@@ -1,31 +1,16 @@
-// const multer  = require('multer');
-
-// module.exports = () => {
-//     const storage = multer.diskStorage({
-//         destination: function (req, file, cb) {
-//             cb(null, `${__dirname}/public/admin/uploads/`)
-//         },
-//         filename: function (req, file, cb) {
-//             const uniqueSuffix = Date.now()
-//             cb(null, `${uniqueSuffix}-${file.originalname}`)
-//         }
-//     })
-//     return storage
-// }
-
-
-const path = require("path");
 const multer  = require('multer');
 
 module.exports = () => {
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, path.join(__dirname, "../public/admin/uploads"));
+            cb(null, `${__dirname}/public/admin/uploads/`)
         },
         filename: function (req, file, cb) {
-            const uniqueSuffix = Date.now();
-            cb(null, `${uniqueSuffix}-${file.originalname}`);
+            const uniqueSuffix = Date.now()
+            cb(null, `${uniqueSuffix}-${file.originalname}`)
         }
-    });
-    return storage;
+    })
+    return storage
 }
+
+
